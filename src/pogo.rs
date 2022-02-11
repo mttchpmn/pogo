4e1bd05b19aba28a0ece58828d2434b2ed7a247c
Matt Chapman <matt@mattchapman.io>
2022-02-11 18:08:29 +1300

    Show foreign keys in describe

--- src/pogo.rs
+++ src/pogo.rs
@@ -1,12 +1,15 @@
+use std::collections::HashMap;
 use std::fmt::Display;
 use std::path::PathBuf;
 
 use postgres::{Client, NoTls, Row};
 use postgres::types::{FromSql, Type};
+use prettytable::row;
 use uuid::Uuid;
 
 use crate::{Loader, Operation};
 
+#[derive(Debug)]
 pub struct PogoResult {
     pub header: Vec<String>,
     pub rows: Vec<Vec<String>>,
@@ -14,7 +17,7 @@ pub struct PogoResult {
 
 pub struct Pogo {
     client: Client,
-    operations: Vec<Operation>
+    operations: Vec<Operation>,
 }
 
 impl Pogo {
@@ -43,7 +46,7 @@ impl Pogo {
             rows.push(row);
         }
 
-        PogoResult { header, rows}
+        PogoResult { header, rows }
     }
 
     pub fn run(&mut self, operation_name: &str) -> PogoResult {
@@ -54,9 +57,9 @@ impl Pogo {
 
     fn get_operation(&self, operation_name: &str) -> Operation {
         // TODO - Remove need to clone in this method
-        let Operation{name, command, description} = &self.operations.iter().find(|x| {x.name == operation_name}).expect("Couldn't find operation");
+        let Operation { name, command, description } = &self.operations.iter().find(|x| { x.name == operation_name }).expect("Couldn't find operation");
 
-        return Operation { name: name.clone(), command: command.clone(), description: description.clone()}
+        return Operation { name: name.clone(), command: command.clone(), description: description.clone() };
     }
 
     fn describe_table(&mut self, table_name: &str) -> PogoResult {
@@ -70,7 +73,57 @@ FROM
 WHERE
    table_name = '{}';", table_name);
 
-        self.run_query(&sql)
+        let mut result = self.run_query(&sql);
+
+        let foreign_keys = self.get_foreign_keys_for_table(table_name);
+        let foreign_key_map = self.map_foreign_keys(foreign_keys);
+
+        let result = self.add_foreign_keys(result, foreign_key_map);
+
+        result
+    }
+
+    fn map_foreign_keys(&self, foreign_keys: PogoResult) -> HashMap<String, String> {
+        let column_name_index = self.get_column_index_by_name("column_name", &foreign_keys.header);
+        let foreign_table_name_index = self.get_column_index_by_name("foreign_table_name", &foreign_keys.header);
+        let foreign_column_name_index = self.get_column_index_by_name("foreign_column_name", &foreign_keys.header);
+
+        let mut result = HashMap::new();
+
+        for row in foreign_keys.rows {
+            let column_name: &str = row.get(column_name_index).unwrap();
+            let foreign_table: &str = row.get(foreign_table_name_index).unwrap();
+            let foreign_column: &str = row.get(foreign_column_name_index).unwrap();
+
+            let value = format!("{}({})", foreign_table, foreign_column);
+
+            result.insert(column_name.to_string(), value);
+        }
+
+        result
+    }
+
+    fn get_column_index_by_name(&self, name: &str, columns: &Vec<String>) -> usize {
+        let (column_name_index, _) = &columns.iter().enumerate().find(|(i, x)| { x.contains(name) }).unwrap();
+
+        column_name_index.to_owned()
+    }
+
+    fn add_foreign_keys(&self, mut rows: PogoResult, keys: HashMap<String, String>) -> PogoResult {
+        rows.header.push("REFERENCES".to_string());
+
+        let column_name_index = self.get_column_index_by_name("column_name", &rows.header);
+
+        for row in &mut rows.rows {
+            let column_name: &str = row.get(column_name_index.to_owned()).unwrap();
+
+            match keys.get(column_name) {
+                Some(foreign_key) => row.push(foreign_key.to_string()),
+                None => ()
+            }
+        }
+
+        rows
     }
 
     fn describe_database(&mut self) -> PogoResult {
@@ -90,6 +143,27 @@ ORDER BY 1,2;
 ";
 
         self.run_query(describe_sql)
+    }
+
+    pub fn get_foreign_keys_for_table(&mut self, table_name: &str) -> PogoResult {
+        let sql = format!("\
+SELECT
+    tc.table_name,
+    kcu.column_name,
+    ccu.table_name AS foreign_table_name,
+    ccu.column_name AS foreign_column_name
+FROM
+    information_schema.table_constraints AS tc
+    JOIN information_schema.key_column_usage AS kcu
+      ON tc.constraint_name = kcu.constraint_name
+      AND tc.table_schema = kcu.table_schema
+    JOIN information_schema.constraint_column_usage AS ccu
+      ON ccu.constraint_name = tc.constraint_name
+      AND ccu.table_schema = tc.table_schema
+WHERE tc.constraint_type = 'FOREIGN KEY' AND tc.table_name='{}'; ", table_name);
+        let result = self.run_query(&sql);
+
+        result
     }
 
     pub fn run_query(&mut self, sql: &str) -> PogoResult {
@@ -123,8 +197,8 @@ ORDER BY 1,2;
             &Type::UUID => self.parse_value::<Uuid>(&row, index),
             &Type::INT4 => self.parse_value::<i32>(&row, index),
             col_type => {
-            // format!("ERR - Couldn't parse type: `{}`", col_type).to_string()
-               self.parse_value::<&str>(row, index)
+                // format!("ERR - Couldn't parse type: `{}`", col_type).to_string()
+                self.parse_value::<&str>(row, index)
             }
         };
 
